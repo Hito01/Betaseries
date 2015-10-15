fdaf7242f95178ed29639cd1b1dc9182b2ee58d2
Nicolas Collard <beauceron@dogstudio.be>
2015-10-15 11:49:32 +0200

    Finished request code and specs

--- spec/betaseries/request_spec.rb
+++ spec/betaseries/request_spec.rb
@@ -3,13 +3,47 @@ require 'spec_helper'
 describe Betaseries::Request do
   describe "#initialize" do
     context "when the api key is not set" do
-    #it "should raise an exception if the api key is not set" do
       it { expect { Betaseries::Request.new('') }.to raise_error(Betaseries::Error::ApiKeyMissing) }
     end
 
     context "when the api key is set" do
-    #it "should return a new request if the api key is set" do 
       it { expect(Betaseries::Request.new(API_KEY)).to_not be_nil }
+    end
+  end
+
+  it "should response to get" do
+    expect(Betaseries::Request.respond_to?(:get)).to be true
+  end
+
+  it "should response to post" do
+    expect(Betaseries::Request.respond_to?(:post)).to be true
+  end
+
+  it "should response to put" do
+    expect(Betaseries::Request.respond_to?(:put)).to be true
+  end
+
+  it "should response to delete" do
+    expect(Betaseries::Request.respond_to?(:delete)).to be true
+  end
+
+  describe "#parse" do
+    context "when the request returns a http code 200" do
+      it "should parse the body" do
+        stub_get('http://foo.bar', '{"foo": "bar"}')
+        response = HTTParty.get('http://foo.bar')
+        request = Betaseries::Request.new(API_KEY)
+        expect(request.parse(response)).to eq({"foo" => "bar"})
+      end
+    end
+
+    context "when the request doesn't return a http code 200" do
+      it "should raise an HttpError" do
+        stub_get('http://foo.bar', "", 404)
+        response = HTTParty.get('http://foo.bar')
+        request = Betaseries::Request.new(API_KEY)
+        expect { request.parse(response) }.to raise_error(Betaseries::Error::HttpError)
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,18 @@ Dir.glob(File.join(File.dirname(__FILE__), '../lib/**/*.rb')).each do |file|
   require file
 end
 
+def stub_get(url, body = "", status = 200)
+  stub_request(:get, url).to_return(body: body, status: status)
+end
+
+def stub_post(url, body = "", status = 200)
+  stub_request(:post, url).to_return(body: body, status: status)
+end
+
+def stub_put(url, body = "", status = 200)
+  stub_request(:put, url).to_return(body: body, status: status)
+end
+
+def stub_delete(url, body = "", status = 200)
+  stub_request(:delete, url).to_return(body: body, status: status)
+end
