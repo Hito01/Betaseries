aaa33c1bb95898e662aa998b048fd8796c887485
Nicolas Collard <nicolas@epic.net>
2015-10-20 07:46:51 +0200

    Add shows display method + specs

--- lib/betaseries/methods/shows.rb
+++ lib/betaseries/methods/shows.rb
@@ -1,8 +1,15 @@
 class Betaseries::Client
   module Shows
 
-    def search(query, options = {})
+    def shows_search(query, options = {})
       @request.get("/shows/search/#{query}", options)
+    end
+
+    def shows_display(h_id, options = {})
+      permitted_ids = ['id', 'thetvdb_id', 'imdb_id']
+      id = h_id.map { |k,v| "#{k}=#{v}" if permitted_ids.include?(k.to_s) }
+      id = id.join('&')
+      @request.get("/shows/display?#{id}", options)
     end
   end
 end
--- spec/betaseries/methods/shows_spec.rb
+++ spec/betaseries/methods/shows_spec.rb
@@ -1,16 +1,54 @@
 require 'spec_helper'
 
 describe Betaseries::Client::Shows do
-  describe "#search" do
-    before do
-      stub_get("#{@client.request.class.base_uri}/shows/search/Gotham", 'shows_search')
-      @shows = @client.search('Gotham')
-    end
+  describe "#shows_search" do
+    context "when there are results" do
+      before do
+        stub_get("#{@client.request.class.base_uri}/shows/search/Gotham", 'shows_search')
+        @shows = @client.shows_search('Gotham')
+      end
 
-    context "when the searched show exist" do
-      it "should return an array with all shows" do
+      it "should return an array with all the matching shows" do
         expect(@shows['shows']).to be_an Array
         expect(@shows['shows'].first['title']).to eq('Gotham')
+      end
+    end
+
+    context "when there isn't result" do
+      before do
+        stub_get("#{@client.request.class.base_uri}/shows/search/Gotham", 'shows_search_empty')
+        @shows = @client.shows_search('Gotham')
+      end
+
+      it "should return an array with all the matching shows" do
+        expect(@shows['shows']).to be_an Array
+        expect(@shows['shows']).to be_empty
+      end
+    end
+  end
+
+  describe "#shows_display" do
+    context "when the requested show exists" do
+      before do
+        stub_get("#{@client.request.class.base_uri}/shows/display?id=7942", 'shows_display')
+        @show = @client.shows_display({id: 7942})
+      end
+
+      it "should return a hash with the show information" do
+        expect(@show['show']).to be_a Hash
+        expect(@show['show']['id']).to eq(7942)
+      end
+    end
+
+    context "when the requested show does not exist" do
+      before do
+        stub_get("#{@client.request.class.base_uri}/shows/display?id=123456", 'shows_display_empty')
+        @show = @client.shows_display({id: 123456})
+      end
+
+      it "should return an error" do
+        expect(@show['show']).to be_nil
+        expect(@show['errors']).to_not be_empty 
       end
     end
   end
